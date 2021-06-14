8f9fe355ce58b65a9a4dd33fb8e7efb41a8e9e95
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-14 17:18:55 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,19 +6,20 @@ public class Main {
     public static void main(String[] args) {
 
         Animal animal = new Animal();
-        animal.AnimalName = "Blackbirds";
+        //animal.AnimalName = "Blackbirds";
+        animal.AnimalDetails = "";
         animal.SomeFunc();
 
         Animal Bird = new Birds();
-        Bird.AnimalName = "Chickadees";
+        //Bird.AnimalName = "Chickadees";
+        Bird.AnimalDetails = "Chickadees";
+        Bird.SomeFunc();
 
         Birds Bird2 = new Birds();
-        Bird2.AnimalName = "Crows";
+        //Bird2.AnimalName = "Crows";
+        Bird2.AnimalDetails = "dfgdfg";
         Bird2.BirdName = "American Crow";
         Bird2.SomeFunc();
-
-        Animal Bird3 = Bird2;
-        Bird3.AnimalName = "";
 
 
     }
