8ab1275fdb5d88687ce93c25f853b8125428ff3a
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-14 17:19:14 +0200

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -2,7 +2,9 @@ package com.company;
 
 public class Animal {
 
-    private String AnimalName = "xxx";
+ private String AnimalName = "xxx";
+
+    protected String AnimalDetails = "yyy";
 
     public final void SomeFunc(){
         System.out.println("parent");
